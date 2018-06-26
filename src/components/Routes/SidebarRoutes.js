a759440885615ff5d1d1b2c9886ae3633aaf1741
mkinfrared <mkinfrared@gmail.com>
2018-06-26 16:48:24 -0600

    changed p tags to link. moved the hashRouter to index.js

--- src/components/Routes/SidebarRoutes.js
+++ src/components/Routes/SidebarRoutes.js
@@ -1,7 +1,9 @@
 import {Switch, Route} from 'react-router-dom';
+// Components
+import Browse from '../Browse/Browse';
 
 export default (
 	<Switch>
-		<Route/>
+		<Route path="/browse" component={Browse}/>
 	</Switch>
 )
