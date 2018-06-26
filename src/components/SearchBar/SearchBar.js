f5b5df2dc5e6ff98b002c618410a2088c93fe609
Jordan Buon <jordanmb803@github.com>
2018-06-26 12:11:56 -0600

    input and buttons added

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,7 +4,11 @@ import "./SearchBar.css";
 class SearchBar extends Component {
     render(){
         return (
-            <div className='searchBar'>SearchBar</div>
+            <div className='searchBar'>
+                <button> back </button>
+                <button> forward </button>
+                <input />
+            </div>
         )
     }
 }
