f99b0c48d53a583f86036667805e0d31e3cbc124
Jordan Buon <jordanmb803@github.com>
2018-06-27 10:13:36 -0600

    removed duplicate

--- src/index.js
+++ src/index.js
@@ -4,17 +4,10 @@ import {HashRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
-import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
-<<<<<<< HEAD
 <HashRouter>
     <App />
 </HashRouter>
     , document.getElementById('root'));
-=======
-	<HashRouter>
-		<App/>
-	</HashRouter>, document.getElementById('root'));
->>>>>>> master
 // registerServiceWorker();
