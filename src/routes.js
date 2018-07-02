af79c20a8eed655ae7a6ad9d5b53b2a6624b3714
Jordan Buon <jordanmb803@github.com>
2018-07-02 14:55:06 -0600

    routes fixed

--- src/routes.js
+++ src/routes.js
@@ -9,14 +9,34 @@ import DashBoard from './components/Dashboard/Dashboard';
 import Playlist from './components/Playlist/Playlist';
 import Category from './components/Category/Category';
 import Album from './components/Album/Album';
+import BrowseNav from './components/BrowseNav/BrowseNav';
+import Overview from './components/Browse/Overview';
+import Charts from './components/Browse/Charts';
+import Genres from './components/Browse/Genres';
+import NewReleases from './components/Browse/NewReleases';
+import Songs from './components/Songs/Songs';
+import Albums from './components/Albums/Albums';
+import Artists from './components/Artists/Artists';
 
 export default (
-    <div>
+    <div className='routes-container'>
         {/* Main Routes */}
         <Route exact path="/" component={Login} />
-        <Route path="/dashboard/browse" component={DashBoard} />
+        <Route path="/dashboard/" component={DashBoard} />
+        <Route path="/dashboard/browse" component={BrowseNav} />
+        <Route path="/dashboard/browse/overview" component={Overview} />
+        <Route path="/dashboard/browse/charts" component={Charts} />
+        <Route path="/dashboard/browse/genres" compnonent={Genres} />
+        <Route path="/dashboard/browse/releases" component={NewReleases} />
+
+
         <Route path="/dashboard/playlist/:ownerId/:playlistId" component={Playlist} />
         <Route path='/dashboard/category/:categoryId/:categoryName' component={Category} />
         <Route path='/dashboard/album/:albumId' component={Album} />
+        <Route path='/dashboard/songs' component={Songs} />
+        <Route path='/dashboard/Albums' component={Albums} />
+        <Route path='/dashboard/Artists' component={Artists} />
+
+
     </div>
 )
