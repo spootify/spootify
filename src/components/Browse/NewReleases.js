bb74676931c9cda45e6517094668de1b16cddde7
mkinfrared <mkinfrared@gmail.com>
2018-07-09 11:23:43 -0600

    added styles for charts, new releases, category

--- src/components/Browse/NewReleases.js
+++ src/components/Browse/NewReleases.js
@@ -1,36 +1,42 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import axios from 'axios';
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 export default class NewRealeses extends Component {
-  constructor() {
-    super()
-    this.state = {
-      newReleases: []
-    }
-  }
+	constructor() {
+		super()
+		this.state = {
+			newReleases: []
+		}
+	}
 
-  componentDidMount() {
-    axios.get('/spotify/browse/newReleases').then(res => {
-      console.log(res)
-      this.setState({
-        newReleases: res.data.body.albums.items
-      })
-    })
-  }
-  render() {
-    console.log(this.state.newReleases)
-    return (
-      <div className='flexRowWrap'>
-        {this.state.newReleases.map(album => {
-          return (
-            <Link to={`/dashboard/album/${album.id}`} key={album.id} ><div>
-              <img src={album.images[1].url} alt='album'/>
-              <p>{album.name}</p>
-            </div></Link>
-          )
-        })}
-      </div>
-    )
-  }
+	componentDidMount() {
+		axios.get('/spotify/browse/newReleases').then(res => {
+			console.log(res)
+			this.setState({
+				newReleases: res.data.body.albums.items
+			})
+		})
+	}
+
+	render() {
+		return (
+			<div className='flexRowWrap new-releases'>
+				<h3>New albums & singles</h3>
+				<hr/>
+				<div className="new-releases-wrapper">
+					{this.state.newReleases.map(album => {
+						return (
+							<Link to={`/dashboard/album/${album.id}`} key={album.id}>
+								<div>
+									<img src={album.images[1].url} alt='album'/>
+									<p>{album.name}</p>
+								</div>
+							</Link>
+						)
+					})}
+				</div>
+			</div>
+		);
+	}
 }
