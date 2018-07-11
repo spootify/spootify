32d6ef015d8c5f565e7890b1911f6f07c04a41ea
mkinfrared <mkinfrared@gmail.com>
2018-07-11 16:41:19 -0600

    moved musicplayer.scss to index.scss import

--- src/components/MusicPlayer/MusicPlayer.js
+++ src/components/MusicPlayer/MusicPlayer.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import axios from 'axios';
-import './MusicPlayer.css';
 
 //Font Awesome
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
