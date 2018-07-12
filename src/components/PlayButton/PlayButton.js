93681ebbfe568453875a20c5e8130d6fda31a225
Jordan Buon <jordanmb803@github.com>
2018-07-11 22:19:24 -0600

    play button pauses and plays music and song list has a working search bar

--- src/components/PlayButton/PlayButton.js
+++ src/components/PlayButton/PlayButton.js
@@ -1,14 +1,41 @@
-import React, {Component } from 'react'
+import React, { Component } from 'react'
+import { playSong, pauseSong, getCurrentlyPlaying } from '../../ducks/player';
+import { connect } from 'react-redux';
+import './PlayButton.css'
 
 class PlayButton extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            songPlaying: ''
+        }
+    }
 
-    render(){
-        return(
+    playSongUpdateCurrentlyPlaying(trackUri, id) {
+        this.props.playSong(this.props.player.deviceID, trackUri);
+        setTimeout(() => this.props.getCurrentlyPlaying(), 500)
+        this.setState({ songPlaying: id })
+    }
+
+    pauseSong() {
+        this.props.pauseSong()
+        this.setState({ songPlaying: '' })
+    }
+
+    render() {
+        console.log(this.props)
+        return (
             <div>
-                <button id='play-button'>Play</button>
+                <button id='play-button' onClick={this.state.songPlaying === '' ? () => this.playSongUpdateCurrentlyPlaying(this.props.uri, this.props.id) : () => this.pauseSong()}>{this.state.songPlaying === '' ? 'PLAY' : 'PAUSE'}</button>
             </div>
         )
     }
 }
 
-export default PlayButton
+export default connect(({ player }) => ({ player }),
+    {
+        playSong,
+        pauseSong,
+        getCurrentlyPlaying
+    })
+    (PlayButton)
--- src/components/SongList/SongList.jsx
+++ src/components/SongList/SongList.jsx
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {playSong, pauseSong, getCurrentlyPlaying} from '../../ducks/player';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { playSong, pauseSong, getCurrentlyPlaying } from '../../ducks/player';
+import { connect } from 'react-redux';
 
 class SongList extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props)
 		this.state = {
 			songPlaying: ''
@@ -16,10 +16,10 @@ class SongList extends Component {
 
 
 	//Methods
-	playSongUpdateCurrentlyPlaying(trackUri, id){
+	playSongUpdateCurrentlyPlaying(trackUri, id) {
 		this.props.playSong(this.props.player.deviceID, trackUri);
 		setTimeout(() => this.props.getCurrentlyPlaying(), 500)
-		this.setState({songPlaying: id})
+		this.setState({ songPlaying: id })
 	}
 
 	pauseSong() {
@@ -30,53 +30,56 @@ class SongList extends Component {
 	}
 
 
-	render(props){
+	render(props) {
 
-	let {tracks} = this.props;
-	tracks = tracks.map((e) => (
-		<tr key={e.track.id}>
-			<td><FontAwesomeIcon
-				icon={this.state.songPlaying === e.track.id ? 'pause-circle' : "play-circle"}
-				onClick={this.state.songPlaying === '' ? () => this.playSongUpdateCurrentlyPlaying(e.track.uri, e.track.id) : () => this.pauseSong()}/>
-			</td>
-			<td>{e.track.name}</td>
-			<td>{e.track.artists.map((artist, i, arr) => (
-				(arr.length > 1) ? (i === arr.length - 1) ? artist.name : artist.name + ', ' : artist.name))}
-			</td>
-			<td>{e.track.album.name}</td>
-			<td>{Math.floor(e.track.duration_ms / 1000 / 60)}:{(e.track.duration_ms / 60 % 60 < 10) ?
-															   '0' + Math.floor(e.track.duration_ms / 60 % 60) :
-															   Math.floor(e.track.duration_ms / 60 % 60)}
-			</td>
-		</tr>
-	));
+		let { tracks } = this.props;
 
-	return (
-		<div className='song-list'>
-			<table>
-				<thead>
-				<tr>
-					<td></td>
-					<td>Title</td>
-					<td>Artist</td>
-					<td>Album</td>
-					<td>Dur</td>
-				</tr>
-				</thead>
-				<tbody>
-				{tracks}
-				</tbody>
-			</table>
-		</div>
-	)
+		tracks = tracks.filter(eF => {
+			return eF.track.name.toLowerCase().includes(this.props.searchInput.toLowerCase())
+		}).map((e) => (
+			<tr key={e.track.id}>
+				<td><FontAwesomeIcon
+					icon={this.state.songPlaying === e.track.id ? 'pause-circle' : "play-circle"}
+					onClick={this.state.songPlaying === '' ? () => this.playSongUpdateCurrentlyPlaying(e.track.uri, e.track.id) : () => this.pauseSong()} />
+				</td>
+				<td>{e.track.name}</td>
+				<td>{e.track.artists.map((artist, i, arr) => (
+					(arr.length > 1) ? (i === arr.length - 1) ? artist.name : artist.name + ', ' : artist.name))}
+				</td>
+				<td>{e.track.album.name}</td>
+				<td>{Math.floor(e.track.duration_ms / 1000 / 60)}:{(e.track.duration_ms / 60 % 60 < 10) ?
+					'0' + Math.floor(e.track.duration_ms / 60 % 60) :
+					Math.floor(e.track.duration_ms / 60 % 60)}
+				</td>
+			</tr>
+		));
+
+		return (
+			<div className='song-list'>
+				<table>
+					<thead>
+						<tr>
+							<td></td>
+							<td>Title</td>
+							<td>Artist</td>
+							<td>Album</td>
+							<td>Dur</td>
+						</tr>
+					</thead>
+					<tbody>
+						{tracks}
+					</tbody>
+				</table>
+			</div>
+		)
+	}
+
 }
 
-}
-
-export default connect(({player}) => ({player}),
+export default connect(({ player }) => ({ player }),
 	{
 		playSong,
 		pauseSong,
 		getCurrentlyPlaying
 	})
-(SongList);
+	(SongList);
